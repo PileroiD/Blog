7e273f6cfc0c4dbd6309521e66779e9220bb008d
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-01-20 21:17:59 -0700

    create main component with routing

---
+++ src/Blog.js
@@ -0,0 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+
+const Content = styled.div`
+    padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+    text-align: center;
+`;
+
+const Header = () => <div>Header</div>;
+const Footer = () => <div>Footer</div>;
+
+function Blog() {
+    return (
+        <>
+            <Header />
+
+            <Content>
+                <H2>Content of page</H2>
+                <Routes>
+                    <Route path="/" element={<div>Main Page</div>} />
+                    <Route path="/login" element={<div>Auth</div>} />
+                    <Route path="/register" element={<div>Registration</div>} />
+                    <Route path="/users" element={<div>Users</div>} />
+                    <Route path="/post" element={<div>New Post</div>} />
+                    <Route path="/post/:post_id" element={<div>Post</div>} />
+                    <Route path="*" element={<div>Error</div>} />
+                </Routes>
+            </Content>
+
+            <Footer />
+        </>
+    );
+}
+
+export default Blog;
